5850e432a94ca5946287dff4c7395b185cf65d1f
shamsWMM <shams.alsaegh@gmail.com>
2024-01-14 00:50:15 +1100

    Imported pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 function App() {
   return (
     <div className="App">
