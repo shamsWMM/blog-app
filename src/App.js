10de314087fc4cce2d1d8502043b102aa04dc017
shamsWMM <shams.alsaegh@gmail.com>
2024-01-14 11:49:47 +1100

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Blog Web Application
-      </h1>
-      <div id="page-body">
-        Welcome to this blog!
+    <BrowserRouter>
+      <div className="App">
+        <h1>
+          Blog Web Application
+        </h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage/>}> </Route>
+            <Route path="/about" element={<AboutPage/>}> </Route>
+            <Route path="/articles" element={<ArticlesListPage/>}> </Route>
+            <Route path="/articles/:articleId" element={<ArticlePage/>}> </Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
