74212e783cac4d45df2b2838d0511de7b135a860
shamsWMM <shams.alsaegh@gmail.com>
2024-01-14 22:30:15 +1100

    Connected front and back end

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0, comments: []
+    });
     const params = useParams;
     //const articleId = params.articleId;
     const { articleId } = params(); //object destructuring
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, []);
+
     const article = articles.find(
         article => article.name === articleId);
 
@@ -15,6 +29,7 @@ const ArticlePage = () => {
     return (
         <>
             <h1> {article.title}.</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s).</p>
             {
                 article.content.map((paragraph, i) => (
                     <p key={i}>{paragraph}</p>
