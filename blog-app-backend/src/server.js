b51f089e972d9de9a67c61a10c5897ea214259f5
shamsWMM <shams.alsaegh@gmail.com>
2024-01-14 14:37:31 +1100

    Added comments endpoint

--- blog-app-backend/src/server.js
+++ blog-app-backend/src/server.js
@@ -2,15 +2,18 @@ import express from 'express';
 
 let articlesInfo = [{
     name: 'learn-react',
-    upvotes: 0
+    upvotes: 0,
+    comments: []
 },
 {
     name: 'learn-node',
-    upvotes: 0
+    upvotes: 0,
+    comments: []
 },
 {
     name: 'mongodb',
-    upvotes: 0
+    upvotes: 0,
+    comments: []
 }
 ];
 
@@ -29,6 +32,21 @@ app.put('/api/articles/:name/upvote', (req, res) => {
        res.send('That article doesn\'t exist.'); 
     }
 });
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name} = req.params;
+    const {postedBy, text} = req.body;
+    const article = articlesInfo.find( 
+        a => a.name === name
+    );
+    if(article){
+        article.comments.push({postedBy,text});
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist.'); 
+    }
+}
+);
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000.');
